4bd2808adb7ace73abb5f42c612979182426d64c
Jordan Olthoff <jordanolthoff@Jordans-MacBook-Pro.local>
2015-11-04 13:42:16 -0800

    Listen for leftGame on server

--- server/Sphero/spheroconnection.js
+++ server/Sphero/spheroconnection.js
@@ -213,6 +213,8 @@ module.exports.init = function(io, socket) {
   });
   socket.on('leftGame', function(){
     activeUsers[this.id].joined = false;
+    console.log("user who submitted left game event is ",activeUsers[this.id]);
+    console.log("active users after left game is ", activeUsers);
   });
 
   socket.on('disconnect', function(){
