63c92571876a419585db5a9407e319b1b4f23d79
Jordan Olthoff <jordanolthoff@Jordans-MacBook-Pro.local>
2015-11-04 14:26:55 -0800

    Remove all socket in room on ended event

--- server/Sphero/spheroconnection.js
+++ server/Sphero/spheroconnection.js
@@ -92,7 +92,6 @@ var single = function(io, data) {
   if (!activeUsers[this.id].joined) {
     var gameId = (1 +Math.random() * 100000).toString();
     this.join(gameId);
-
     activeUsers[this.id].joined = gameId;
 
     playersInRoom[gameId] = [];
@@ -207,7 +206,7 @@ module.exports.init = function(io, socket) {
   });
   socket.on('leftGame', function() {
     console.log("user who submitted left game event is ", activeUsers[this.id].joined);
-    this.leave(activeUsers[this.id].joined)
+    this.leave(activeUsers[this.id].joined);
     activeUsers[this.id].joined = false;
 
   });
